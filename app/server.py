fa537d4aa9ea26838298698ef6233fe08d38066e
Andrea <ravazziniandrea@hotmail.it>
2023-03-23 23:12:52 +0000

    Added methods to recive image

--- app/server.py
+++ app/server.py
@@ -5,17 +5,31 @@ import uvicorn
 
 app = FastAPI()
 
-#@app.post("/files/")
-#async def create_file(file: bytes = File()):
-#    return {"file_size": len(file)}
-
 @app.post("/files/")
-async def create_file(img: UploadFile = File(...)):
-    with open("files/newgatto.jpg","wb") as buff:
-        shutil.copyfileobj(img.file, buff)
-        
-    print("Arrivato, grande: "+img.filename)
-    return {"filename": img.filename}
+async def salva_file(img: UploadFile = File(...)):
+    salvata = salva_filesystem(img)
+    scritta_db = salva_db(img.name)
+
+def salva_filesystem(img):
+    try:
+        with open("files/nome_data_foto.jpg","wb") as buffer:
+            shutil.copyfileobj(img.file, buffer)
+        print("File scritto: "+img.name)
+        return true
+    except:
+        print("Errore scrittura file!!")
+        return False
+
+def salva_db(path):
+    #TODO 
+    return True
+
+#async def create_file(img: UploadFile = File(...)):
+#    with open("files/newgatto.jpg","wb") as buff:
+#        shutil.copyfileobj(img.file, buff)
+#        
+#    print("Arrivato, grande: "+img.filename)
+#    return {"filename": img.filename}
 
 
 @app.get("/")
@@ -24,5 +38,5 @@ async def root():
 
 
 if __name__ == "__main__":
-    print("Avvio server-")
+    print("-- Avvio server --")
     uvicorn.run("server:app", host="0.0.0.0", port=10481, workers=3)
