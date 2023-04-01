b2b6de8166679b223df5a79c61aa1054282f0b5f
andrea <ravazziniandrea@hotmail.it>
2023-04-01 19:27:11 +0200

    Added .sql initialization

--- database_creation.sql
+++ database_creation.sql
@@ -1,18 +1,18 @@
 
-CREATE TABLE IF NOT EXISTS Photo(
+CREATE TABLE Photo(
 	id SERIAL,
 	localPath VARCHAR(255),
 	downloaded BOOLEAN DEFAULT FALSE,
 	digested VARCHAR(255) NOT NULL,
 	sentTime TIMESTAMP,
 	PRIMARY KEY (localpath)
-)
+);
 
-CREATE TABLE IF NOT EXISTS DownloadTime(
+CREATE TABLE DownloadTime(
 	id SERIAL NOT NULL,
 	localPath VARCHAR(255),
 	downloadTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT fk_photo
 		FOREIGN KEY(localPath) 
 			REFERENCES Photo(localPath)
-)
+);
