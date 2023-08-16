1909e8eb89e078c20c4bb94275d403b24353736f
Simone Santillo <simone.santillo@leitha.eu>
2023-08-16 16:59:27 +0200

    Improved image format when downloading

--- app/constants.py
+++ app/constants.py
@@ -3,5 +3,5 @@ from pathlib import Path
 CONFIG_PATH = "./app/config"
 KEY_PATH = f"{CONFIG_PATH}/key"
 PHOTO_FOLDER = Path("./photos")
-MEDIA_TYPE = "image/jpeg"
+MEDIA_TYPE = "image/png"
 EXTENSION = ".png"
